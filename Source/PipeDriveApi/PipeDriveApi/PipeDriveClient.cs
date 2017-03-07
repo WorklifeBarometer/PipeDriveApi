d878fa5ade921b5e2bcbb6875196d6166831a21c
Aleksey Shubin <aleksey@shubin.eu>
2017-03-07 19:09:39 +0200

    Removed entity services from the client to avoid tons of generics in the client definition.

--- Source/PipeDriveApi/PipeDriveApi/PipeDriveClient.cs
+++ Source/PipeDriveApi/PipeDriveApi/PipeDriveClient.cs
@@ -1,40 +1,13 @@
-﻿using PipeDriveApi.EntityServices;
-using PipeDriveApi.Serializer;
+﻿using PipeDriveApi.Serializer;
 using RateLimiter;
 using RestSharp;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PipeDriveApi
 {
-    public class PipeDriveClient : PipeDriveClient<Person, Organization, Deal, Product, Activity, Note>
-    {
-        public PipeDriveClient(string apiKey) : base(apiKey)
-        {
-        }
-    }
-
-    public class PipeDriveClient<TPerson, TOrganization, TDeal, TProduct> 
-        : PipeDriveClient<TPerson, TOrganization, TDeal, TProduct, Activity, Note>
-        where TPerson : Person
-        where TOrganization : Organization
-        where TDeal : Deal
-        where TProduct : Product
-    {
-        public PipeDriveClient(string apiKey) : base(apiKey) { }
-    }
-
-    public class PipeDriveClient<TPerson, TOrganization, TDeal, TProduct, TActivity, TNote> : IPipeDriveClient
-        where TPerson : Person
-        where TOrganization : Organization
-        where TDeal : Deal
-        where TProduct : Product
-        where TActivity : Activity
-        where TNote : Note
+    public class PipeDriveClient : IPipeDriveClient
     {
         private readonly RestClient _Client;
         private readonly string _ApiToken;
@@ -53,13 +26,6 @@ namespace PipeDriveApi
 
             // Fly under the radar by doing only 99 requests per 10 seconds
             _TimeContraint = TimeLimiter.GetFromMaxCountByInterval(99, TimeSpan.FromSeconds(10));
-
-            Persons = new PersonEntityService<TPerson>(this);
-            Organizations = new OrganizationEntityService<TOrganization>(this);
-            Deals = new DealEntityService<TDeal>(this);
-            Products = new ProductEntityService<TProduct>(this);
-            Activities = new ActivityEntityService<TActivity>(this);
-            Notes = new NoteEntityService<TNote>(this);
         }
 
         public async Task ExecuteRequestAsync(IRestRequest request)
@@ -105,13 +71,5 @@ namespace PipeDriveApi
                 }
             });
         }
-
-
-        public PersonEntityService<TPerson> Persons { get; private set; }
-        public OrganizationEntityService<TOrganization> Organizations { get; private set; }
-        public DealEntityService<TDeal> Deals { get; private set; }
-        public ProductEntityService<TProduct> Products { get; private set; }
-        public ActivityEntityService<TActivity> Activities { get; private set; }
-        public NoteEntityService<TNote> Notes { get; private set; }
     }
 }
