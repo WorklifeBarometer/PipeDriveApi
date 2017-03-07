579d184f1f840f9333d2e7cd05ef7a06eabb65de
Aleksey Shubin <aleksey@shubin.eu>
2017-03-07 19:13:04 +0200

    Removed PipeDriveValueProvider - caused errors in custom fields, doesn't seem to be actually needed (?)

--- Source/PipeDriveApi/PipeDriveApi/Serializer/PipeDriveContractResolver.cs
+++ Source/PipeDriveApi/PipeDriveApi/Serializer/PipeDriveContractResolver.cs
@@ -1,9 +1,4 @@
 ﻿using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Reflection;
 
@@ -11,7 +6,7 @@ namespace PipeDriveApi.Serializer
 {
     public class PipeDriveContractResolver : DefaultContractResolver
     {
-        public PipeDriveContractResolver() : base()
+        public PipeDriveContractResolver()
         {
             NamingStrategy = new SnakeCaseNamingStrategy();
         }
@@ -25,43 +20,6 @@ namespace PipeDriveApi.Serializer
                 prop.PropertyName = cfa.FieldApiKey;
             }
             return prop;
-        }
-
-        protected override IValueProvider CreateMemberValueProvider(MemberInfo member)
-        {
-            var cfa = member.GetCustomAttribute<CustomFieldAttribute>(true);
-            if (cfa != null)
-            {
-                return new PipeDriveValueProvider(member, cfa.FieldApiKey);
-            }
-            return base.CreateMemberValueProvider(member);
-        }
-    }
-
-    public class PipeDriveValueProvider : IValueProvider
-    {
-        private readonly MemberInfo _memberInfo;
-        private readonly string _fieldApiKey;
-
-        public PipeDriveValueProvider(MemberInfo memberInfo, string fieldApiKey)
-        {
-            _memberInfo = memberInfo;
-            _fieldApiKey = fieldApiKey;
-        }
-
-        public void SetValue(object target, object value)
-        {
-            target.GetType().GetProperty(_memberInfo.Name).SetValue(target, value);
-        }
-
-        /// <summary>
-        /// Gets the value.
-        /// </summary>
-        /// <param name="target">The target to get the value from.</param>
-        /// <returns>The value.</returns>
-        public object GetValue(object target)
-        {
-            return target;
         }
     }
 }
